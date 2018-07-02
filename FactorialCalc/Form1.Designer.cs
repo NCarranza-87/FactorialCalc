025e2b89d79f0f2cf3375755eb84600ab6799224
NCarranza-87 <36281270+NCarranza-87@users.noreply.github.com>
2018-07-02 15:47:08 -0700

    Add project files.

---
+++ FactorialCalc/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace FactorialCalc
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.lblFactorial = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtFactor = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Location = new System.Drawing.Point(73, 84);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(75, 23);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(162, 84);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(13, 28);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(47, 13);
+            this.lblNumber.TabIndex = 2;
+            this.lblNumber.Text = "Number:";
+            // 
+            // lblFactorial
+            // 
+            this.lblFactorial.AutoSize = true;
+            this.lblFactorial.Location = new System.Drawing.Point(13, 54);
+            this.lblFactorial.Name = "lblFactorial";
+            this.lblFactorial.Size = new System.Drawing.Size(50, 13);
+            this.lblFactorial.TabIndex = 3;
+            this.lblFactorial.Text = "Factorial:";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(73, 20);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(18, 20);
+            this.txtNumber.TabIndex = 4;
+            // 
+            // txtFactor
+            // 
+            this.txtFactor.Location = new System.Drawing.Point(73, 47);
+            this.txtFactor.Name = "txtFactor";
+            this.txtFactor.Size = new System.Drawing.Size(164, 20);
+            this.txtFactor.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(267, 125);
+            this.Controls.Add(this.txtFactor);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.lblFactorial);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "Form1";
+            this.Text = "Factorial Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.Label lblFactorial;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtFactor;
+    }
+}
+
---
+++ FactorialCalc/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FactorialCalc
+{
+    public partial class Form1 : Form
+    {
+        /*****************************************
+         * This program is a factorial calculator
+         * it will ask the user to enter a number
+         * betwee 1 and 20.
+         *****************************************/
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            /***************************************
+             * define variables for program, one
+             * to get number from user and anoher
+             * to calculate factor for the entered
+             * number the user enters
+             ***************************************/
+
+            //number has a datatype of int,
+            //will allow for user to enter a number
+            int number = 1;
+
+            //factror has a datatype of long,
+            //will be the number to calculate
+            //against number
+            long factor = 1;
+
+            //asks for the user to enter
+            //a number
+            number = int.Parse(txtNumber.Text);
+
+            //Validate for the vaiable number
+            if (number < 0)     
+            {              //validates for the condition of 
+                           //any negatives have been entered
+                MessageBox.Show("Please enter a positive number!");
+            }
+            else if (number == 0)       
+            {                      //validates for the condition if
+                                   //the user enters a zero
+                MessageBox.Show("Please enter a number between 1 and 20!");
+
+            }
+            else if (number > 20)
+            {                     //validates for the condition if
+                                  //the user enters a number
+                                  //greater than 20
+                MessageBox.Show("Please enter a number 20 or less!");
+            }
+            else
+            {
+                //for loop to calculate the factor
+                //the user enters a number
+                for (int i = number; i >= 1; i--)
+                {
+                    factor = factor * i;
+                }
+
+                //displays the factor in txtFactor
+                txtFactor.Text = factor.ToString();
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            /******************************************
+             * This method is wired to the Exit button
+             * to close the program
+             ******************************************/
+
+            //this keyword will invoke the close method
+            //to close the program
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            /******************************************
+             * This method is wired to the form itself
+             ******************************************/
+
+            //on the txtFactor will invoke the ReadOnly
+            //and is set to true, the user will not be
+            //able to enter a number
+            txtFactor.ReadOnly = true;
+        }
+    }
+}
